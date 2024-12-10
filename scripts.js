7f565062f4c5ae258a9a2427b6e4462b7249a453
Kubashen Naidoo <kubashen.naidoo1@lexisnexis.com.au>
2024-12-10 14:13:21 +1100

    updated script

--- scripts.js
+++ scripts.js
@@ -22,31 +22,6 @@ const repoNameMapping = {
     'Terraform-Starter': 'Terraform Basics'
 };
 
-
-
-// Your GitHub username
-const username = 'kubashen'; // Replace with your GitHub username
-
-// GitHub API URL to fetch the repositories
-const apiUrl = `https://api.github.com/users/${username}/repos`;
-
-// List of repository names to exclude
-const excludedRepos = [
-    'kubashennaidoo.github.io',  // Example repo to exclude
-    'DS_1'                       // Another repo to exclude
-];
-
-// Mapping of GitHub repo names to custom display names
-const repoNameMapping = {
-    'Ansible-Starter': 'Ansible Basics',
-    'Data-Processing-with-Spark': 'Data Processing with Apache Spark',
-    'Data-Visualisation-Time-Series': 'Time Series Data Visualisation',
-    'DS_1': 'Data Science Project 1',
-    'kubashennaidoo.github.io': 'Personal Portfolio Website',
-    'Starter-HTML-CSS-JS': 'HTML, CSS, and JavaScript Starter',
-    'Terraform-Starter': 'Terraform Basics'
-};
-
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
